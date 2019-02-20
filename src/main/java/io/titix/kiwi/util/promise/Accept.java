b596d044730a0e89cdebbc628b937a008a71fd59
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2019-02-20 17:01:48 +0400

    Revert "Add promise"
    This reverts commit 556537e8

--- src/main/java/io/titix/kiwi/util/promise/Accept.java
+++
@@ -1,9 +0,0 @@
-package io.titix.kiwi.util.promise;
-
-/**
- * @author Tigran.Sargsyan on 20-Feb-19
- */
-public interface Accept<T> {
-
-	void accept(T obj);
-}
--- src/main/java/io/titix/kiwi/util/promise/PromiseResolver.java
+++
@@ -1,9 +0,0 @@
-package io.titix.kiwi.util.promise;
-
-/**
- * @author Tigran.Sargsyan on 20-Feb-19
- */
-public interface PromiseResolver<T> {
-
-	void resolve(Accept<T> accept, Reject reject);
-}
