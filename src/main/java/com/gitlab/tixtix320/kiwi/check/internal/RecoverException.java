8ea00b84766e75b0d64d38f7e26c9f44201c19ce
tix32 <tixtix320@gmail.com>
2019-04-30 00:54:06 +0400

    Add recover exception message.

--- src/main/java/com/gitlab/tixtix320/kiwi/check/internal/RecoverException.java
+++ src/main/java/com/gitlab/tixtix320/kiwi/check/internal/RecoverException.java
@@ -3,6 +3,6 @@ package com.gitlab.tixtix320.kiwi.check.internal;
 public class RecoverException extends RuntimeException {
 
 	public RecoverException(Throwable cause) {
-		super(cause);
+		super("See cause", cause);
 	}
 }
