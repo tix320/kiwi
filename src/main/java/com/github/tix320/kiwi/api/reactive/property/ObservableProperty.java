f89052633dbdf6849510d6a511686f927269a2b0
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-04-19 15:17:36 +0400

    Add property helper methods.

--- src/main/java/com/github/tix320/kiwi/api/reactive/property/ObservableProperty.java
+++ src/main/java/com/github/tix320/kiwi/api/reactive/property/ObservableProperty.java
@@ -5,4 +5,12 @@ import com.github.tix320.kiwi.api.reactive.ObservableCandidate;
 public interface ObservableProperty<T> extends ObservableCandidate<T> {
 
 	T getValue();
+
+	default boolean isEmpty() {
+		return getValue() == null;
+	}
+
+	default boolean isPresent() {
+		return getValue() != null;
+	}
 }
